1417d41cf866356bc2264f8b71e24017a434de0b
Grant <chobanyangrant@gmail.com>
2021-07-21 15:49:14 +0300

    release

--- front-ts/src/index.tsx
+++ front-ts/src/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+// import {BrowserRouter} from 'react-router-dom'
+
 import App from './App';
+// import createSagaMiddleware from "redux-saga";
+// import { Provider } from "react-redux";
+// import { createStore, applyMiddleware } from "redux";
+// import { composeWithDevTools } from "redux-devtools-extension";
+
+// const sagaMiddleware = createSagaMiddleware();
+// const store = createStore(
+//   reducer,
+//   composeWithDevTools(applyMiddleware(sagaMiddleware))
+// );
+// sagaMiddleware.run(wotcher);
 
 ReactDOM.render(
   <React.StrictMode>
+      {/* <Provider store={store}></Provider> */}
+    {/* <BrowserRouter> */}
     <App />
+    {/* </BrowserRouter> */}
+    {/* </Provider> */}
   </React.StrictMode>,
   document.getElementById('root')
 );
