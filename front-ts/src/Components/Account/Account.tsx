42c95370339432d8cd7dfc276f52734cff3e038b
Grant <chobanyangrant@gmail.com>
2021-07-23 14:10:19 +0300

    release:3

--- front-ts/src/Components/Account/Account.tsx
+++ front-ts/src/Components/Account/Account.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector} from "react-redux";
 
 function Account() {
+  const login = useSelector(state => state)
+ 
   return (
     <div>
       Account
+      {/* <h1>HELLO {state.userReducer.user.login}</h1> */}
     </div>
   );
 }
