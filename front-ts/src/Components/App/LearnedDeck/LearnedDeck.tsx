c767eeea5ee03943b4edd4e1e2efbbb583e8c4fe
anboch <a.g.bochka@gmail.com>
2021-07-27 21:58:59 +0300

    завершение игры

--- front-ts/src/Components/App/LearnedDeck/LearnedDeck.tsx
+++ front-ts/src/Components/App/LearnedDeck/LearnedDeck.tsx
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { resultOfAnswerSagaAC } from '../../../redux/ActionCreators/card/resultOfAnswerSagaAC';
 import { CardType } from '../../../redux/types/card/cardTypes';
 import { DeckType } from '../../../redux/types/deck/deckTypes';
@@ -9,9 +10,11 @@ export default function LearnedDeck({ deckInGame }: { deckInGame: DeckType }) {
   const [showAnswer, setShowAnswer] = useState(false);
   const [position, setPosition] = useState(0);
   const dispatch = useDispatch();
+  const history = useHistory();
 
   const deckInGameId = deckInGame._id;
   const cardInGame = deckInGame.readyToRepeat[position];
+
   const resultOfAnswerHandler = (
     deckInGameId: string,
     cardInGame: CardType,
@@ -24,15 +27,18 @@ export default function LearnedDeck({ deckInGame }: { deckInGame: DeckType }) {
     );
   };
 
+  useEffect(() => {
+    if (position === deckInGame.readyToRepeat.length - 1)
+      history.push('/account');
+  }, [position, deckInGame.readyToRepeat.length, history]);
+
   return (
     <>
       <Card
         className="text-center "
         style={{ width: '500px', height: '400px' }}
       >
-        <Card.Header>
-          {position + 1}/{deckInGame.readyToRepeat.length}
-        </Card.Header>
+        <Card.Header>{deckInGame.title}</Card.Header>
         <Card.Body className="d-flex align-items-center justify-content-between flex-column">
           {/* <Card.Title>Вопрос</Card.Title> */}
           <Card.Text>{deckInGame.readyToRepeat[position].question}</Card.Text>
@@ -73,13 +79,16 @@ export default function LearnedDeck({ deckInGame }: { deckInGame: DeckType }) {
             </div>
           )}
         </Card.Body>
-        <Card.Footer className="d-flex text-muted">
+        <Card.Footer className="d-flex justify-content-between text-muted">
           <Button
             variant="link"
             onClick={() => setPosition((pre) => (pre > 0 ? (pre -= 1) : pre))}
           >
             предыдущая карта
           </Button>
+          <div className="d-flex align-items-center">
+            {position + 1}/{deckInGame.readyToRepeat.length}
+          </div>
         </Card.Footer>
       </Card>
     </>
