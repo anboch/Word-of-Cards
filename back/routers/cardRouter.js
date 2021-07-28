e626aad8ea09fa5ab48e85e9ff356b3dbf510ac4
anboch <a.g.bochka@gmail.com>
2021-07-28 09:53:11 +0300

    add catch in card router

--- back/routers/cardRouter.js
+++ back/routers/cardRouter.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const Card = require('../bd/cardShema');
 const Deck = require('../bd/deckShema');
 
-
 // Получить результат
 // isLogin добавить!
 router.route('/result').post(async (req, res) => {
@@ -37,12 +36,16 @@ router.route('/result').post(async (req, res) => {
 });
 router.route('/').post(async (req, res) => {
   const { question, answer } = req.body;
-  
+
   try {
     const newCard = await Card.create({
       question,
-      answer
+      answer,
     });
-    return res.status(200).json({newCard} );
+    return res.status(200).json({ newCard });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
+});
 
 module.exports = router;
