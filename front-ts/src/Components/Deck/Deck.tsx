e12438874002713b6c9e31d78f09bb7d38bab241
Grant <chobanyangrant@gmail.com>
2021-07-30 18:53:38 +0300

    release:final1

--- front-ts/src/Components/Deck/Deck.tsx
+++ front-ts/src/Components/Deck/Deck.tsx
@@ -136,12 +136,12 @@ export default function Deck({ deck }: { deck: DeckType }) {
         {del ? (
           <Card.Body>
             {(deck.readyToRepeat?.length > 0 ||
-              deck.notStarted?.length > 0) && 
-                // <div className="instrument">
+              deck.notStarted?.length > 0) && (
+                
               <Button variant="success" onClick={() => startGameHandler(deck)}>
                 Учить
               </Button>
-           <span> {' '}
+            )}{' '}
             <img
               onClick={() => thisDeck(deck)}
               style={{ cursor: 'pointer', width: '40px', margin: '2px' }}
@@ -154,7 +154,6 @@ export default function Deck({ deck }: { deck: DeckType }) {
               className="del"
               src="/del.jpg"
             />
-            }
           </Card.Body>
         ) : (
           <div>
@@ -162,7 +161,7 @@ export default function Deck({ deck }: { deck: DeckType }) {
               Вы деиствительно хотите удалить колоду?
             </h5>
             <div className="buttonDel">
-              <Button onClick={() => setDel((pre)=>!pre)} variant="dark">
+              <Button onClick={() => setDel(true)} variant="dark">
                 Отменить
               </Button>
               <Button onClick={() => delDeck(deck._id)} variant="danger">
